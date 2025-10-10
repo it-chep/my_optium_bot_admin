24c05367b7b893f61b76fa5f97294c5b2e245eb3
1ww1s <ctop124@mail.ru>
2025-10-10 16:26:16 +0300

    fix. медиа фалй не обяз (параметр content_type_id)

--- src/features/newsletterDataChange/ui/action/Action.tsx
+++ src/features/newsletterDataChange/ui/action/Action.tsx
@@ -31,7 +31,7 @@ export const Action: FC<IProps> = ({formError, setFormError}) => {
         const error: IFormError<INewsletterData>[] = [];
         let isOk = true;
         for(let key in newsletterData){
-            if(key as keyof INewsletterData === 'media_id') continue
+            if(key as keyof INewsletterData === 'content_type_id') continue
             if(newsletterData[key as keyof INewsletterData] === '' || 
                 newsletterData[key as keyof INewsletterData] === -1 || 
                 ((key as keyof INewsletterData === 'users_lists') && newsletterData['users_lists'].length === 0) 
